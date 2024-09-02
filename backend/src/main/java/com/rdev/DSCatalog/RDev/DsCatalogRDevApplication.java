e59b5cea0879e949c5fcc6f26374ab3b8680138d
Lima-Ricardo <ricardoalexdel@gmail.com>
2024-09-01 23:00:15 -0300

    Backend project created

---
+++ backend/src/main/java/com/rdev/DSCatalog/RDev/DsCatalogRDevApplication.java
@@ -0,0 +1,13 @@
+package com.rdev.DSCatalog.RDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsCatalogRDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsCatalogRDevApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rdev/DSCatalog/RDev/DsCatalogRDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdev.DSCatalog.RDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsCatalogRDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
